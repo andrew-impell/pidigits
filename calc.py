45865f282ae8ad86f0525d2848b61768113262a5
snubriot <sk8rgtar@yahoo.com>
2013-05-16 09:28:34 -0300

    Update calc.py
    implemented Gauss-Legendre algorithm

--- calc.py
+++ calc.py
@@ -1,11 +1,28 @@
 """
 5/8/13
-A simple program to calculate the digits of pi 
+A simple program to calculate the digits of pi
+inital commit
+5/16/13
 """
+from math import sqrt
 
-ra = int(raw_input("Enter size: ")
+n = int(raw_input("Enter size: ")
 
-count = 0
+#inital walues
 
-while count < ra:
+a, b, t, p = 1, 1/sqrt(2), 1/4, 1
+
+def calc_pi(a,b,t,p,n):
+    count = 0
+    while count < n:
+        a = an
+        a = (a + b)/2
+        b = sqrt(a*b)
+        t = t-(p*((an-a)**2))
+        p = 2*p
+        count += 1
+    pi = ((a+b)**2)/(4*t)
+    return pi
+
+print calc_pi(a,b,t,p,n)
     
